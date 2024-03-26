e2f875951600d7f96eb220c8e94396af1bcd8cd8
S571495 <ys.sara2068@gmail.com>
2024-03-26 13:28:58 -0500

    created new pug named grid.pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ornamentsRouter = require('./routes/ornaments');
+var gridRouter = require('./routes/grid');
 
 
 var app = express();
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ornaments', ornamentsRouter);
-
+app.use('/grid', gridRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
